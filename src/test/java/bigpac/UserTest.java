1840844b4d58360b483255a68bb55e0f32f59220
FabienPinard <pinard.fabien@gmail.com>
2017-02-06 15:43:42 +0100

    Commit 1

---
+++ src/test/java/bigpac/UserTest.java
@@ -0,0 +1,24 @@
+package bigpac;
+import org.hamcrest.core.IsEqual;
+import org.junit.Assert;
+import org.junit.Test;
+
+import bigpac.User;
+
+public class UserTest {
+
+    @Test
+    public void test1() {
+        User user = new User();
+        user.setFirstName("Jean");
+//        Assert.assertThat(user.getLastName(), IsEqual.equalTo("Jean"));
+    }
+
+    @Test
+    public void test2() {
+        User user = new User();
+        user.setLastName("Dupont");
+//        Assert.assertThat(user.getLastName(), IsEqual.equalTo("Dupont"));
+    }
+
+}
